21c97c38690b9e8af5b33106097136bb14e65321
noman171GH <noman171gh@yahoo.com>
2024-10-30 00:19:08 +0100

    Product list visible

--- src/context/data/MyState.jsx
+++ src/context/data/MyState.jsx
@@ -1,6 +1,12 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import MyContext from "./MyContext";
+
+ import { fireDB }  from "../../firebase/FirebaseConfig";
+ import { Timestamp, addDoc, collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+ import { toast } from 'react-toastify';
+
+
 
 function MyState(props) {
   // -------Here we were testing that usestate is functional or not-----------------------------------------------------
@@ -27,10 +33,105 @@ function MyState(props) {
 
 //--------------------------------------------------------------------------------------------------------------
 
-// -------------------------its about toast ----Loading----------------------------------
+// -------------------------its about Loader (mouse busy) ----Loading----------------------------------
   const [loading, setLoading] = useState(false);
 // ---------------------------------------------------------------------
 
+
+
+// ----------------------------Dealing With Product----------------------------------------------------
+//-------------------------------------------------------------------------------------------------------
+//------- An Object state which we will use to add products ---------------------------
+const [products, setProducts] = useState({
+  title: null,
+  price: null,
+  imageUrl: null,
+  category: null,
+  description: null,
+  time: Timestamp.now(),
+  date: new Date().toLocaleString(
+    "en-US",
+    {
+      month: "short",
+      day: "2-digit",
+      year: "numeric",
+    }
+  )
+
+})
+// --------------------------------------------------------------------------------------------------------
+
+// ********************** Add Product **********************
+  const addProduct = async () => {
+    if (products.title == null || products.price == null || products.imageUrl == null || products.category == null || products.description == null) {
+      return toast.error('Please fill all fields')
+    };
+    const productRef = collection(fireDB, "products");
+    setLoading(true);
+    try {
+      await addDoc(productRef, products);
+      toast.success("Product Added successfully");
+      // to show toast and than navigate ...setTimeout
+      setTimeout(() => {
+        window.location.href = '/dashboard'
+                        }, 800);
+      getProductData();
+
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      alert (error);
+      setLoading(false);
+    }
+    //setProducts("");
+  }
+// ********************** End of ---Add Product **********************
+
+
+
+// ************************** get product********************************
+
+const [product, setProduct] = useState([]); // using a state to get product data
+
+const getProductData = async () => {
+  setLoading(true)
+  try {
+
+    // here we are writing query where we are teling that from FB bring tblProducts order by time 
+    const q = query(
+      collection(fireDB, "products"),
+      orderBy("time")
+    );
+
+    // You can listen to a document with the onSnapshot() method.
+    // An initial call using the callback you provide creates a document snapshot
+    // immediately with the current contents of the single document.
+    // Then, each time the contents change, another call updates the document snapshot.
+    // so its all about "real time updated data".
+
+
+    const data = onSnapshot(q, (QuerySnapshot) => {
+      let productArray = [];
+      QuerySnapshot.forEach((doc) => {
+        productArray.push({ ...doc.data(), id: doc.id }); // What is ...doc ? = spread Operator https://www.w3schools.com/howto/howto_js_spread_operator.asp
+      });                                                  // we are saving Data and id in our array "productsArray"
+      setProduct(productArray)                           // then assigning "productArray" to our state "Product"
+      setLoading(false);
+    });
+    return () => data;     /// we are returning data got by snapshot
+  } catch (error) {
+    console.log(error)
+    setLoading(false)
+  }
+}
+
+useEffect(() => {        // putting it in useEffect so we can fetch data automatically. its [] , so it will run once atlaest.
+  getProductData();
+}, []);
+
+// ************************** End of -----get product********************************
+
+// ----------------------------End of ---Dealing With Product----------------------------------------------------
 
   return (
     // -------Here we were testing that usestate is functional or not-----------------------------------------------------
@@ -41,8 +142,11 @@ function MyState(props) {
     // </>
     //-----------------------------------------------------------------------------------------------------------------------
 
+
+
+    // passing States to other places where we need it
     <>
-      <MyContext.Provider value={{ mode, toggleMode , loading, setLoading }}>
+      <MyContext.Provider value={{ mode, toggleMode , loading, setLoading, products, setProducts,addProduct,product }}>
         {props.children}
       </MyContext.Provider>
     </>
--- src/pages/admin/pages/AddProduct.jsx
+++ src/pages/admin/pages/AddProduct.jsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
+import MyContext from "../../../context/data/MyContext";
 
 function AddProduct() {
+
+  const context = useContext(MyContext);
+  const {products,setProducts,addProduct} = context;
+
+
   return (
     <div>
       <div className=" flex justify-center items-center h-screen">
         <div className=" bg-gray-800 px-10 py-10 rounded-xl ">
           <div className="">
             <h1 className="text-center text-white text-xl mb-4 font-bold">
-              Add Product
+              Add Product 
             </h1>
+            <br /> <h2 className="text-center text-white text-xl mb-4 font-italic">Committed Sin as opened with refresh without Navigate</h2>
           </div>
           <div>
             <input
               type="text"
+              onChange={(e) => setProducts({ ...products, title: e.target.value })}
               name="title"
               className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
               placeholder="Product title"
@@ -21,6 +29,7 @@ function AddProduct() {
           <div>
             <input
               type="text"
+              onChange={(e) => setProducts({ ...products, price: e.target.value })}
               name="price"
               className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
               placeholder="Product price"
@@ -29,6 +38,7 @@ function AddProduct() {
           <div>
             <input
               type="text"
+              onChange={(e) => setProducts({ ...products, imageUrl: e.target.value })}
               name="imageurl"
               className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
               placeholder="Product imageUrl"
@@ -37,6 +47,7 @@ function AddProduct() {
           <div>
             <input
               type="text"
+              onChange={(e) => setProducts({ ...products, category: e.target.value })}
               name="category"
               className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
               placeholder="Product category"
@@ -47,12 +58,13 @@ function AddProduct() {
               cols="30"
               rows="10"
               name="title"
+              onChange={(e) => setProducts({ ...products, description: e.target.value })}
               className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
-              placeholder="Product title"
+              placeholder="Product Desc"
             ></textarea>
           </div>
           <div className=" flex justify-center mb-3">
-            <button className=" bg-pink-500 w-full text-black font-bold  px-2 py-2 rounded-lg">
+            <button onClick={ addProduct } className=" bg-pink-500 w-full text-black font-bold  px-2 py-2 rounded-lg">
               Add Product
             </button>
           </div>
